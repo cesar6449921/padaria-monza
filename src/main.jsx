ba25c7eecab2914d99c55e592b4009d314fba4e4
unknown <julio29236@gmail.com>
2025-12-04 17:56:32 -0300

    Feita merge de branch

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App';
 import Lojas from './pages/Lojas.jsx';
 import Cardapio from './pages/Cardapio.jsx';
 import './styles.css';
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/lojas",
+    element: <Lojas />,
+  },
+  {
+    path: "/cardapio",
+    element: <Cardapio />,
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/lojas" element={<Lojas />} />
-        <Route path="/cardapio" element={<Cardapio />} />
-      </Routes>
-    </BrowserRouter>
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
